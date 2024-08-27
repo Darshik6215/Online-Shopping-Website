ef998983162bf4f8a80e04836bd45b80e119b8bb
delta <125557501+Darshik6215@users.noreply.github.com>
2024-08-27 17:39:09 +0530

    products

---
+++ api/data.js
@@ -0,0 +1,22 @@
+let kidsProduct = [
+  {
+    url: "https://sslimages.shoppersstop.com/sys-master/images/h2f/h9d/32567601528862/S24ASBFZ86367GR_GREEN.jpg_2000Wx3000H",
+    name: "Hellcat",
+    content: "Printed Cotton Knee Length Boys Co-Ord Set",
+    price: 600,
+  },
+  {
+    url: "https://sslimages.shoppersstop.com/sys-master/images/h3e/h69/30864226615326/A23711BTRACK06_RED.jpg_2000Wx3000H",
+    name: "Life",
+    content: "Color Block Cotton Regular Fit Boys Track Pants",
+    price: 899,
+  },
+  {
+    url: "https://sslimages.shoppersstop.com/sys-master/images/hf7/h27/32776249081886/S24710BSHORT33_NAVY.jpg_2000Wx3000H",
+    name: "Stop",
+    content: "Solid Cotton Regular Fit Boys Shorts",
+    price: 50,
+  },
+];
+
+module.exports = kidsProduct;
--- index.js
+++ index.js
@@ -2,18 +2,39 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8080;
+// const data = require("./api/data");
 
 // middleware:
 app.use(express.urlencoded({ extended: true }));
+// public folder static file:
+app.use(express.static(path.join(__dirname, "public")));
 // ejs:
 app.set("view engine", "ejs");
 // views folder path:
 app.set("views", path.join(__dirname, "views"));
-// public folder static file:
-app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
+});
+
+app.get("/men", (req, res) => {
+  res.render("men");
+});
+
+app.get("/women", (req, res) => {
+  res.render("women");
+});
+
+app.get("/kids", (req, res) => {
+  res.render("kids");
+});
+
+app.get("/watch", (req, res) => {
+  res.render("watch");
+});
+
+app.get("*", (req, res) => {
+  res.render("error");
 });
 
 app.listen(port, () => {
